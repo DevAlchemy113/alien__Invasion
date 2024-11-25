2f79f1f2f4fcd7c96bfe98b516973ee9b968e1c1
DevAlchemy113 <david.sil503@gmail.com>
2024-11-25 11:27:22 -0600

    stop the ship from disspeering
    upfated the coded stop the ship from going to the sides and dissapearinf

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,17 +28,37 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type ==pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            self.clock.tick(60)
+
             #Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
+    def _check_events(self):
+            """Respond to keypresses and mouse events."""
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                      sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
 
-            # Make the most recently drawn screeen visible.
-            pygame.display.flip()
-            self.clock.tick(60)
+    def _update_screen(self):
+         """Update images on the screen, and flip to the new screen."""
+         self.screen.fill(self.settings.bg_color)
+         self.ship.blitme()
+
+         pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instancem and run the game.
